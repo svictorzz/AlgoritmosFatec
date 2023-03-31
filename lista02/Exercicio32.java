cc1e4167cf730552b43d6831ba60cbb7be539d5e
Victor Souza <127612160+svictorzz@users.noreply.github.com>
2023-03-31 15:37:59 -0300

    Add files via upload

---
+++ lista02/Exercicio32.java
@@ -0,0 +1,22 @@
+package listas.lista02;
+/*
+ Escrever um programa que leia o codigo do produto escolhido do cardapio de uma lan- 
+chonete e a quantidade. O programa deve calcular o valor a ser pago por aquele lanche.
+Considere que a cada execucao somente será calculado um pedido. O cardapio da lan-
+chonete segue o padrao abaixo: 
+
+Especificação      Código      Preço
+Cachorro Quente     100        1.20
+Bauru Simples       101        1.30
+Bauru com Ovo       102        1.50
+Hamburguer          103        1.20
+Cheeseburguer       104        1.70
+Suco                105        2.20
+Refrigerante        106        1.00
+ */
+import java.util.Scanner;
+public class Exercicio32 {
+    public static void main(String[] args) {
+        
+    }
+}
