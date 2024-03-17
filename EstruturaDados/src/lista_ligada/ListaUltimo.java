1e319a7c88828c37d06a9e32859524afa5a2c6c1
Victor Souza <127612160+svictorzz@users.noreply.github.com>
2024-03-17 19:30:32 -0400

    Add files via upload

--- EstruturaDados/src/lista_ligada/ListaUltimo.java
+++ EstruturaDados/src/lista_ligada/ListaUltimo.java
@@ -14,6 +14,18 @@ public class ListaUltimo extends Lista{
 		}
 		super.setPrimeiro(novo);
 	}
-	
-	
+
+	@Override
+	public void insereFim(int i) {
+		No novo = new No(i, null);
+		if (estaVazia()) {
+			super.setPrimeiro(novo);
+		} else {
+			No aux = super.getPrimeiro();
+			while(aux.getProximo() != null) {
+				aux = aux.getProximo();
+			}
+			aux.setProximo(novo);
+		}
+	}
 }
