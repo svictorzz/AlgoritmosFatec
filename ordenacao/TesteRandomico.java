7b3e3bb03eb3dd7a97e37b627e5a3eb7db1c3a81
Victor Souza <127612160+svictorzz@users.noreply.github.com>
2024-03-17 19:31:51 -0400

    Add files via upload

---
+++ ordenacao/TesteRandomico.java
@@ -0,0 +1,9 @@
+package ordenacao;
+
+public class TesteRandomico {
+	public static void main(String[] args) {
+		NossoVetor vetor = new NossoVetor(6);
+		vetor.preencheVetor();
+		System.out.println(vetor);
+	}
+}
