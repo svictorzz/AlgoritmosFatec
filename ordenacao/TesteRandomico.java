cd2d7ff81a9630eb58024707d795c53dec80c533
Victor Souza <127612160+svictorzz@users.noreply.github.com>
2024-03-17 20:33:39 -0300

    Delete ordenacao directory

--- ordenacao/TesteRandomico.java
+++
@@ -1,9 +0,0 @@
-package ordenacao;
-
-public class TesteRandomico {
-	public static void main(String[] args) {
-		NossoVetor vetor = new NossoVetor(6);
-		vetor.preencheVetor();
-		System.out.println(vetor);
-	}
-}
